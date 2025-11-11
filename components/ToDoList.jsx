f54c25d6039f7284c86762c16ca93ecb29642dd0
Noah Zschogner <noahzschogner@gmail.com>
2025-11-11 13:55:56 -0700

    Updated all files for Lab 3
    I made sure to render App in index.tsx this time, modifies App.jsx, and dynamically rendered tasks in ToDoList.

--- components/ToDoList.jsx
+++ components/ToDoList.jsx
@@ -1,33 +1,28 @@
 import { StyleSheet, Text, View, ScrollView, Pressable } from 'react-native'
 import React from 'react'
 
-const ToDoList = () => {
+const ToDoList = ({ tasks = []}) => {
   return (
-    <View>
+    <View style={styles.container}>
       <ScrollView>
-        <Pressable>
-          <View style={[styles.task, styles.completed]}>
-            <Text style={styles.taskText}>Do laundry</Text>
+        {tasks.map((tasks, index) => (
+          <View key={index.toString()} style={styles.task}>
+            <Text style={styles.taskText}>{tasks}</Text>
           </View>
-        </Pressable>
-        <Pressable>
-          <View style={[styles.task]}>
-            <Text style={styles.taskText}>Go to gym</Text>
-          </View>
-        </Pressable>
-        <Pressable>
-          <View style={[styles.task, styles.completed]}>
-            <Text style={styles.taskText}>Walk dog</Text>
-          </View>
-        </Pressable>
+        ))}
       </ScrollView>
     </View>
-  )
+  );
 }
 
 export default ToDoList
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 12,
+    paddingTop: 12,
+  },
     task: {
     padding: 10,
     borderBottomWidth: 1,
